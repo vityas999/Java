be2004ffbc70879c60a257d6de15f07050e8a12a
Vitalik <vitalijzaxarov99@mail.ru>
2023-06-29 22:19:05 +0300

    Task1

---
+++ Task1.java
@@ -0,0 +1,34 @@
+// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();
+
+        int triangleNumber = calculateTriangleNumber(n);
+        int factorial = calculateFactorial(n);
+
+        System.out.println("n-ое треугольное число: " + triangleNumber);
+        System.out.println("n!: " + factorial);
+    }
+
+    public static int calculateTriangleNumber(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int calculateFactorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
